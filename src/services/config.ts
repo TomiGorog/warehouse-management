53f978656e1b4be2b08f899e8f0dd736664410b7
TomiGorog <tamas.peter.gorog@gmail.com>
2023-05-09 15:47:14 +0200

    bug?

---
+++ src/services/config.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASEURL,
+  projectId: import.meta.env.PROJECTID,
+  storageBucket: import.meta.env.STORAGEBUCKET,
+  messagingSenderId: import.meta.env.MESSAGINGSENDERID,
+  appId: import.meta.env.APPID
+};
+
+
+export const app = initializeApp(firebaseConfig)
+export const  database = getDatabase(app);
