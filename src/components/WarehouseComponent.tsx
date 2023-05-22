85a866e732b00b8b63ca91fc14000039674a0b2b
TomiGorog <tamas.peter.gorog@gmail.com>
2023-05-22 16:41:21 +0200

    tbc

--- src/components/WarehouseComponent.tsx
+++ src/components/WarehouseComponent.tsx
@@ -2,11 +2,17 @@ import { useContext } from "react"
 import { closeWarehouse } from "../services/whManagementService"
 import { NoTitleWHProps } from "../types"
 import { PackageContext } from "../context/packageContext"
+import { closeThisWarehouse } from "../services/closeService"
 
 
 const WarehouseComponent: React.FC<NoTitleWHProps> = ( {name, maxCapacity, currentCapacity, state, packages}: NoTitleWHProps) => {
 
-  const {warehouses} = useContext(PackageContext)
+  const {warehouses,setRefetch} = useContext(PackageContext)
+
+  const closeWh = async () => {
+    const whClosed = await closeThisWarehouse(whInfo, warehouses)
+
+  }
   return (
     <div>
       <h2>Name: {name}</h2>
@@ -15,13 +21,13 @@ const WarehouseComponent: React.FC<NoTitleWHProps> = ( {name, maxCapacity, curre
       <p>Warehouse state: {state}</p>
       <button onClick={() => {
         const whInfo: NoTitleWHProps = {
-          name,
-          maxCapacity,
           currentCapacity,
-          state,
-          packages
+          maxCapacity,
+          name,
+          packages,
+          state
         }
-        closeWarehouse(whInfo, warehouses)
+        closeThisWarehouse(whInfo, warehouses)
       }} >Close warehouse</button>
     </div>
   )
---
+++ src/services/closeService.ts
@@ -0,0 +1,36 @@
+import { NoTitleWHProps } from "../types";
+
+export const closeThisWarehouse = (currentWarehouse: NoTitleWHProps, allWarehouses: NoTitleWHProps[]) => {
+    console.log(currentWarehouse, allWarehouses)
+    let warehousesWOutClosing = allWarehouses.filter(warehouse => warehouse.name !== currentWarehouse.name)
+    let packagesToManipulate = currentWarehouse.packages
+    let packagesLeft = []
+    for (let i = 0; i < warehousesWOutClosing.length; i++) {
+        if(warehousesWOutClosing[i].state !== 'full' && warehousesWOutClosing[i].state !== 'closed' && warehousesWOutClosing[i].name !== currentWarehouse.name) {
+            if(warehousesWOutClosing[i].currentCapacity + currentWarehouse.currentCapacity <= warehousesWOutClosing[i].maxCapacity) {
+                warehousesWOutClosing[i].currentCapacity = warehousesWOutClosing[i].currentCapacity + currentWarehouse.currentCapacity;
+                warehousesWOutClosing[i].packages = [...warehousesWOutClosing[i].packages, ...packagesToManipulate];
+                currentWarehouse.state = 'closed';
+                currentWarehouse.currentCapacity = 0;
+                currentWarehouse.packages = []
+                if(warehousesWOutClosing[i].currentCapacity === warehousesWOutClosing[i].maxCapacity) {
+                    warehousesWOutClosing[i].state = 'full';
+                }
+            } else {
+                const numToExtract = warehousesWOutClosing[i].maxCapacity -  warehousesWOutClosing[i].currentCapacity
+                const whereToSlice = currentWarehouse.currentCapacity - numToExtract
+                const packagesToAdd = packagesToManipulate.slice(0, whereToSlice)
+                packagesLeft = packagesToManipulate.slice(whereToSlice)
+                warehousesWOutClosing[i].packages = [...warehousesWOutClosing[i].packages, ...packagesToAdd];
+                packagesToManipulate = packagesLeft
+                warehousesWOutClosing[i].state = 'full';
+                currentWarehouse.packages = packagesLeft
+            } 
+        } else {
+            console.log("full")
+            
+        }
+    }
+    allWarehouses = [currentWarehouse, ...warehousesWOutClosing]
+    console.log(allWarehouses)
+}
